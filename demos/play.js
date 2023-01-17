2ad623d2770b31aec5afbdf63fcffc766105674c
Franco Rosa <km115.franco@gmail.com>
2023-01-17 00:27:48 -0500

    Add video player and image commands

--- demos/play.js
+++ demos/play.js
@@ -1,2 +1,52 @@
 // The following functions are used to play a video or display an
 // image on full screen, then stop the video or stop the full screen image display
+
+
+///////////////////////////////////////////////////////
+///////////////      Video              ///////////////
+///////////////////////////////////////////////////////
+const mpvAPI = require("node-mpv");
+// where you want to initialize the API
+const options = {
+  audio_only: false,
+  auto_restart: true,
+  binary: null,
+  debug: false,
+  ipcCommand: null,
+  socket: "/tmp/node-mpv.sock", // UNIX
+  time_update: 1,
+  verbose: false,
+};
+const mpv = new mpvAPI(options, ["--fullscreen"]);
+
+// somewhere within an async context
+// starts MPV
+mpv
+  .start()
+  .then(() => {
+    return mpv.load("../media/vid1.mp4");
+  })
+  .then(() => {
+    return mpv.getDuration();
+  })
+  .then((duration) => {
+    console.log(duration);
+    return mpv.getProperty("someProperty");
+  })
+  .then((property) => {
+    console.log(property);
+  })
+  // catches all possible errors from above
+  .catch((error) => {
+    // Maybe the mpv player could not be started
+    // Maybe the video file does not exist or couldn't be loaded
+    // Maybe someProperty is not a valid property
+    console.log(error);
+  });
+
+
+///////////////////////////////////////////////////////
+///////////////      Image              ///////////////
+///////////////////////////////////////////////////////
+eog img1.png --fullscreen
+(Send kill AbortSignal, before launching next image)
--- readme.md
+++ readme.md
@@ -2,8 +2,21 @@
 
 > Syncronized video players using udp broadcast messages over the network
 
+## Requirements
+
+mpv player is required, could be installed with:
+
+```bash
+sudo apt install mpv
+sudo apt install eog
+```
+
 ## Folders
 
 ### Demos
 
-Demos contains simple code snippeds to provide basic examples about functions that are integrated in the main script.
+Demos contains simple code snippets to provide basic examples about functions that are integrated in the main script.
+
+## Additional documentation
+
+- https://github.com/j-holub/Node-MPV
