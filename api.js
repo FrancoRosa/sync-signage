e798554591e38a067a3b6af3cb3c1cdb0a39433b
Franco Rosa <km115.franco@gmail.com>
2023-01-18 09:15:20 -0500

    speed up on every loop

---
+++ api.js
@@ -0,0 +1,94 @@
+const dgram = require("dgram");
+const sys = require("child_process");
+
+const multicast = "225.0.0.1";
+const id = Date.now();
+const fullscreen = false;
+const playlist = [
+  { video: true, file: "vid1.mp4", timeout: 3, timed: true },
+  { video: false, file: "img1.png", timeout: 5, timed: true },
+  { video: true, file: "vid2.mp4", timeout: 5, timed: true },
+  { video: false, file: "img2.png", timeout: 5, timed: true },
+];
+
+let current;
+
+const mediaDir = "media/";
+
+const s = dgram.createSocket("udp4");
+
+s.bind(8001, () => {
+  s.addMembership(multicast);
+  console.log("... listening");
+});
+
+s.on("message", (msg, rinfo) => {
+  console.log(`${rinfo.address}:${rinfo.port} >> ${msg}`);
+  const other = JSON.parse(msg);
+  if (other.id < id) {
+    if (other.file != current.file) {
+      if (current.video) stopVideo();
+      else hideImage();
+    }
+  }
+});
+
+const broadcast = (msg) => {
+  s.send(JSON.stringify({ id, msg }), 8001, multicast);
+};
+
+const stopVideo = () => {
+  console.log("killing video");
+  sys.execSync(`pkill -12 mpv`);
+};
+
+const hideImage = () => {
+  console.log("killing image");
+  sys.execSync(`pkill -12 eog`);
+};
+
+const playVideo = (file, timed, timeout) => {
+  return new Promise((resolve, reject) => {
+    if (timed) {
+      console.log("timed video");
+      setTimeout(() => {
+        stopVideo();
+        resolve(1);
+      }, timeout * 1000);
+    }
+    sys.exec(`mpv ${mediaDir + file} ${fullscreen && "--fullscreen"}`, () =>
+      resolve(1)
+    );
+  });
+};
+
+const showImage = (file, timed, timeout) => {
+  return new Promise((resolve, reject) => {
+    if (timed) {
+      console.log("timeout image");
+      setTimeout(() => {
+        hideImage();
+        resolve(1);
+      }, timeout * 1000);
+    }
+    sys.exec(`eog ${mediaDir + file} ${fullscreen && "--fullscreen"}`, () =>
+      resolve(1)
+    );
+  });
+};
+
+// handle playList
+const handlePlaylist = async (playlist) => {
+  for (const element of playlist) {
+    const { video, file, timed, timeout } = element;
+    current = element;
+    broadcast(element);
+    if (video) {
+      await playVideo(file, timed, timeout);
+    } else {
+      await showImage(file, timed, timeout);
+    }
+  }
+};
+
+handlePlaylist(playlist);
